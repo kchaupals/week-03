de91fccecc11333f28d397206841e82e874b6132
Kārlis Čaupals <74049486+kchaupals@users.noreply.github.com>
2026-02-20 15:24:14 +0200

    feat: data collections B task implementation - dictionaries

--- data_collections.py
+++ data_collections.py
@@ -21,9 +21,24 @@ for number in numberList: # For Loop that goes trough list and summs up all the 
 
 averageNum = round(numberSum / listCount, 1)
 
-print('*** A daļas uzdevumu izvade: ***')
+print('*** --- Saraksti ---  ***')
 print(f'*** Summa: {numberSum}, Vidējais: {averageNum}')
 print(f'*** Pāra skaitļi: {evenNumList}')
 print(f'*** Nepāra skaitļi: {oddNumList}')
 print(f'*** Pirmie 3: {numberList[slice(0,3)]}, Pēdējie 2: {numberList[slice(-2, None)]}') # Outputs first 3, Last 2 elements from list 
 print(f'*** Katrs 2: {numberList[slice(None, None, 2)]}') # Outputs elements gap by two from list
+
+# B part of the task
+
+gradeList = {"Anna":85,"Jānis":72,"Līga":95}
+gradeList["Valters"] = 92 # Adds new student to dictionary
+gradeList.pop("Līga") # Removes "Līga" from dictionary
+gradeList["Līga"] = 95 # Adds "Līga" back to dictionary
+gradeList["Jānis"] = 75 # Changes value of "Jānis" in dictionary
+
+for name, grade in gradeList.items():
+    print(f'{name} : {grade}')
+
+topStud = max(gradeList, key=gradeList.get) # Finds the highest value grade in dictionary returns entry name
+topGrade = gradeList[topStud] # Returns grade value for topStud
+print(f'Labākais students: {topStud} ({topGrade})')
