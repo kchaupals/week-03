8300be607aa7585ed379059779fcd05eb041c0b2
Kārlis Čaupals <74049486+kchaupals@users.noreply.github.com>
2026-02-20 14:51:34 +0200

    feat: data collections A part task implementation init

---
+++ data_collections.py
@@ -0,0 +1,29 @@
+# A part of the task
+
+numberList = [1,2,3,4,5,6]
+
+numberList.append(7) # Adds int 7 to end of the list
+numberList.append(12) # Adds int 12 after int 7
+numberList.pop(0) # Removes index 0 value from list
+
+# Sets variables for the loop
+numberSum = 0 # Sets the starting SUM as 0
+listCount = 0 # Sets the list count as 0
+evenNumList = [] # Creates empty list for even numbers
+oddNumList = [] # Creates empty list for odd numbers
+for number in numberList: # For Loop that goes trough list and summs up all the int values 
+    numberSum += number
+    listCount = listCount + 1
+    if number %2 == 0:
+        evenNumList.append(number)
+    else:
+        oddNumList.append(number)
+
+averageNum = round(numberSum / listCount, 1)
+
+print('*** A daļas uzdevumu izvade: ***')
+print(f'*** Summa: {numberSum}, Vidējais: {averageNum}')
+print(f'*** Pāra skaitļi: {evenNumList}')
+print(f'*** Nepāra skaitļi: {oddNumList}')
+print(f'*** Pirmie 3: {numberList[slice(0,3)]}, Pēdējie 2: {numberList[slice(-2, None)]}') # Outputs first 3, Last 2 elements from list 
+print(f'*** Katrs 2: {numberList[slice(None, None, 2)]}') # Outputs elements gap by two from list
